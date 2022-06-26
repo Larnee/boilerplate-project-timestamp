586410327ded00c41e831c86787ed780c40099a7
larnee <larnee@mailfence.com>
2022-06-27 00:53:48 +0200

    Solution 1.6

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.get("/api/:date?", function (req, res) {
 	const unixDate = new Date(date);
 
 	if (unixDate instanceof Date && !isNaN(unixDate)) {
-		res.json({ unix: unixDate.getTime(), utc: unixDate()});
+		const utcDate = unixDate.toUTCString();
+		res.json({ unix: unixDate, utc: utcDate.toUTCString()});
 	}
 	else {
 		res.json({ error : "Invalid Date" });	
